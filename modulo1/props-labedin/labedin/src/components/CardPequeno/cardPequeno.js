c917ee5ddcc38a382a000fd0311acec2e854f44c
Joely Brito <joely_brito@outlook.com>
2022-05-26 20:22:54 -0300

    Adicionado styled components e card pequeno

--- modulo1/props-labedin/labedin/src/components/CardPequeno/cardPequeno.js
+++ modulo1/props-labedin/labedin/src/components/CardPequeno/cardPequeno.js
@@ -1,41 +1,32 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div`
+const CardPequenoContainer = styled.div`
   display: flex;
   align-items: center;
   border: 1px solid black;
   padding: 20px 10px;
   margin-bottom: 10px;
-  height: 200px;
 
   > img {
     width: 70px;
     margin-right: 10px;
     border-radius: 50%;
   }
-
-  > h4 {
-    margin-bottom: 15px;
-  }
-
-  > div {
-    display: flex;
-    flex-direction: column;
-    justify-items: flex-start;
-  }
 `;
 
-function CardGrande(props) {
+function CardPequeno(props) {
   return (
-    <Container>
+    <CardPequenoContainer>
       <img src={props.imagem} />
       <div>
-        <h4>{props.nome}</h4>
-        <p>{props.descricao}</p>
+        <p>
+          <strong>{props.tipo}</strong>
+          {props.texto}
+        </p>
       </div>
-    </Container>
+    </CardPequenoContainer>
   );
 }
 
-export default CardGrande;
+export default CardPequeno;
