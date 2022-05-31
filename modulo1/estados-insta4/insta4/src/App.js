930b830e250453da47858e19562a3bd03145ef20
Joely Brito <joely_brito@outlook.com>
2022-05-31 11:20:22 -0300

    Adicionado novos cards

---
+++ modulo1/estados-insta4/insta4/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+import Post from "./components/Post/Post";
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+function App() {
+  return (
+    <MainContainer>
+      <Post
+        nomeUsuario={"paulinha"}
+        fotoUsuario={"https://picsum.photos/50/50"}
+        fotoPost={"https://picsum.photos/200/150"}
+      />
+
+      <Post
+        nomeUsuario={"Ricardo"}
+        fotoUsuario={"https://i.pravatar.cc/50?img=6"}
+        fotoPost={"https://picsum.photos/200/150"}
+      />
+
+      <Post
+        nomeUsuario={"Matheus"}
+        fotoUsuario={"https://i.pravatar.cc/50?img=9"}
+        fotoPost={"https://picsum.photos/200/150"}
+      />
+    </MainContainer>
+  );
+}
+
+export default App;
