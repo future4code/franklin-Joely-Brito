3dd2ebb016d9c9bfbb31079cb9d9044326fad613
Joely Brito <joely_brito@outlook.com>
2022-05-31 11:29:08 -0300

    Adicionado imagens aos cards

--- modulo1/estados-insta4/insta4/src/App.js
+++ modulo1/estados-insta4/insta4/src/App.js
@@ -14,19 +14,19 @@ function App() {
       <Post
         nomeUsuario={"paulinha"}
         fotoUsuario={"https://picsum.photos/50/50"}
-        fotoPost={"https://picsum.photos/200/150"}
+        fotoPost={"https://picsum.photos/200/150?random=1"}
       />
 
       <Post
         nomeUsuario={"Ricardo"}
         fotoUsuario={"https://i.pravatar.cc/50?img=6"}
-        fotoPost={"https://picsum.photos/200/150"}
+        fotoPost={"https://picsum.photos/200/150?random=2"}
       />
 
       <Post
         nomeUsuario={"Matheus"}
         fotoUsuario={"https://i.pravatar.cc/50?img=9"}
-        fotoPost={"https://picsum.photos/200/150"}
+        fotoPost={"https://picsum.photos/200/150?random=3"}
       />
     </MainContainer>
   );
