65070652533cc61b719f1f4eec3b026006f6f3bc
Joely Brito <joely_brito@outlook.com>
2022-06-27 19:34:49 -0300

    Adicionado autenticação e inputs controlados na pagina de login

--- modulo1/projeto-labex/projeto-labex/src/pages/LoginPage.js
+++ modulo1/projeto-labex/projeto-labex/src/pages/LoginPage.js
@@ -1,22 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Home } from "../pages/HomePage";
 import { Form } from "../pages/ApplicationFormPage";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const navigate = useNavigate()
+  const onChangeEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const onChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const navigate = useNavigate();
   const goToAdminHome = () => {
-    navigate("/admin/trips/list")
-  }
+    navigate("/admin/trips/list");
+  };
+
+  const logar = (event) => {
+    event.preventDefault()
+
+    const body = {
+      email: email,
+      password: password,
+    };
+    axios
+      .post(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labeX/franklin/login",
+        body
+      )
+      .then((response) => {
+        alert("Você esta logado");
+        console.log(response);
+      })
+      .catch((error) => {
+        alert("Não foi possível logar");
+        console.log(error)
+      });
+  };
+
   return (
     <Home>
       <h1>Login</h1>
       <Form>
-        <input placeholder="Login" type={"email"} />
-        <input placeholder="Senha" type={"password"} />
-        <button onClick={goToAdminHome}>Entrar</button>
+        <input
+          placeholder="Login"
+          type={"email"}
+          value={email}
+          onChange={onChangeEmail}
+        />
+        <input
+          placeholder="Senha"
+          type={"password"}
+          value={password}
+          onChange={onChangePassword}
+        />
+        <button onClick={logar}>Entrar</button>
       </Form>
     </Home>
   );
