abec64e42c8dcebb1e1ddae9c2ec3063e5a03086
Joely Brito <joely_brito@outlook.com>
2022-07-19 22:21:34 -0300

    Adicionado teste que checa se a palavra e um palíndromo

--- modulo2/testes-unitarios/testes-unitarios/src/exercicios/ex2.test.js
+++ modulo2/testes-unitarios/testes-unitarios/src/exercicios/ex2.test.js
@@ -5,4 +5,24 @@ describe("Checa Palíndromo", () => {
     const ehPalindromo = checaPalindromo("mirim");
     expect(ehPalindromo).toEqual(true);
   });
+
+  it("retorna true para 'arara'", () => {
+    const ehPalindromo = checaPalindromo("arara");
+    expect(ehPalindromo).toEqual(true);
+  });
+
+  it("retorne true para 'asa'", () => {
+    const ehPalindromo = checaPalindromo("asa");
+    expect(ehPalindromo).toEqual(true);
+  });
+
+  it("retorne false para 'Socorram-me subi no onibus em marrocos'", () => {
+    const ehPalindromo = checaPalindromo("Socorram-me subi no onibus em marrocos");
+    expect(ehPalindromo).toEqual(false);
+  });
+
+  it("retorne false para 'Adias a data da saída'", () => {
+    const ehPalindromo = checaPalindromo("Adias a data da saída");
+    expect(ehPalindromo).toEqual(false);
+  })
 });
