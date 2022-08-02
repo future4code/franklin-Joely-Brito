7acac684b885899754524e69e14d9732c89d3479
Joely Brito <joely_brito@outlook.com>
2022-08-02 20:37:53 -0300

    Adicionado cor a mensagem de retorno do console

--- modulo3/node-package-json/exercicio1/exercicio1.js
+++ modulo3/node-package-json/exercicio1/exercicio1.js
@@ -1,7 +1,16 @@
-
 // Utilizando a propiedade process.argv conseguimos passar os parametros para determinadas funções usando o terminal
-const nome = process.argv[2]
-const idade = Number(process.argv[3])
-const soma = Number(idade + 7)
+const nome = process.argv[2];
+const idade = (process.argv[3]);
+const soma = (Number(idade) + 7);
 
-console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${soma}`)
+if (nome === undefined) {
+  return console.log('\033[0;31m%s\x1b[0m', 'Por favor digite seu nome!')
+}
+
+if (idade === undefined) {
+    return console.log('\033[0;31m%s\x1b[0m', 'Por favor digite seu nome!')
+  //return console.log('%c Por favor digite sua idade!', 'color: red;');
+}
+console.log('\x1b[33m%s\x1b[0m',
+  `Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${soma}`
+);
