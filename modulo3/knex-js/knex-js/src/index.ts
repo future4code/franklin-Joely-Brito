0536fab2f2c975e750a8113d46878defd530116a
Joely Brito <joely_brito@outlook.com>
2022-08-24 14:01:21 -0300

    Adicionado endpoints

--- modulo3/knex-js/knex-js/src/index.ts
+++ modulo3/knex-js/knex-js/src/index.ts
@@ -1,60 +1,98 @@
-import { Request, Response } from "express"
-import connection from "./connection"
-import app from "./app"
-
+import { Request, Response } from "express";
+import connection from "./connection";
+import app from "./app";
 
 const getActorById = async (id: string): Promise<any> => {
   const result = await connection.raw(`
     SELECT * FROM Actor WHERE id = '${id}'
-  `)
+  `);
 
-	return result[0][0]
-}
+  return result[0][0];
+};
 
 const getActorByName = async (name: string): Promise<any> => {
-    const result = await connection.raw(`
+  const result = await connection.raw(`
     SELECT * FROM Actor WHERE name = '${name}'
-    `)
+    `);
 
-    return result[0][0]
-}
+  return result[0][0];
+};
 
 const getActorByGender = async (gender: string): Promise<any> => {
-    const result = await connection.raw(`
+  const result = await connection.raw(`
     SELECT * FROM Actor WHERE gender = '${gender}'
-    `)
-    return result[0]
-}
+    `);
+  return result[0];
+};
 
-app.get("/users/:id", async (req: Request, res: Response) => {
+const updateActorSalary = async (id: string, salary: number): Promise<any> => {
+  await connection("Actor")
+    .update({
+      salary: salary,
+    })
+    .where("id", id);
+};
+
+const deleteActor = async (id: string): Promise<void> => {
+  await connection("Actor").delete().where("id", id);
+};
+
+const avgSalary = async (gender: string): Promise<any> => {
+  const result = await connection("Actor")
+    .avg("salary as average")
+    .where({ gender });
+  return result[0].average;
+};
+
+app.get("/actor/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const actor = await getActorById(id);
+
+    res.status(200).send(actor);
+  } catch (err) {
+    res.status(400).send({
+      message: "erro ao buscar dados",
+    });
+  }
+}); // bata no http://localhost:3003/users/001 e veja o que acontece no terminal
+
+app.get("/actor?gender=", async (req: Request, res: Response) => {
+  try {
+    const gender = req.params.gender;
+    const count = await getActorByGender(gender);
+    res.status(200).send({
+      quantity: count,
+    });
+  } catch (err) {
+    res.status(400).send({
+      message: "Error ao buscar os dados",
+    });
+  }
+});
+
+app.put("/actor/:id", async (req: Request, res: Response) => {
   try {
     const id = req.params.id
-    console.log("aqui")
-    console.log(await getActorById(id))
-
-    res.end()
-  } catch (error) {
-    res.status(500).send("Unexpected error")
+    const salary = req.body.salary
+    await updateActorSalary(id, salary)
+    res.status(200).send({
+      message: "Atualização concluída"
+    })
+  } catch (err){
+    res.status(400).send({
+      message: "Error ao realizar a atualização"
+    })
   }
-}) // bata no http://localhost:3003/users/001 e veja o que acontece no terminal
-
-app.get("/users/name/:name", async (req: Request, res: Response) => {
-    try {
-        const name = req.params.name
-        console.log(await getActorByName(name))
-        res.end()
-    } catch (eror) {
-        res.status(500).send("unexpected error")
-    }
 })
 
-app.get("/users/gender/:gender", async (req: Request, res:Response) => {
-    try {
-        const gender = req.params.gender
-        console.log(gender)
-        console.log(await getActorByGender(gender))
-        res.end()
-    } catch (error) {
-        res.status(500).send("unexpected error")
-    }
+app.delete("/actor/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id
+    await deleteActor(id)
+  } catch {
+    res.status(400).send({
+      message: "Error ao deletar ator!"
+    })
+  }
 })
