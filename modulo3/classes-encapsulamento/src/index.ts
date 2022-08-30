19ce09dfb03f0f2c312a0a268c4a09c71bd70e8d
Joely Brito <joely_brito@outlook.com>
2022-08-30 12:44:33 -0300

    Adicionado arquivos que contém a lógica da atividade

---
+++ modulo3/classes-encapsulamento/src/index.ts
@@ -0,0 +1,21 @@
+class UserAccount {
+    private cpf: string;
+    private name: string;
+    private age: number;
+    private balance: number = 0;
+    // private transactions: Transaction[] = [];
+  
+    constructor(
+       cpf: string,
+       name: string,
+       age: number,
+    ) {
+       console.log("Chamando o construtor da classe UserAccount")
+       this.cpf = cpf;
+       this.name = name;
+       this.age = age;
+    }
+  
+  }
+
+  const cadastro = new UserAccount("0000000", "Joely", 18);
