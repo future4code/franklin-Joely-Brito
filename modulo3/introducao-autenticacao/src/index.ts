c759eed65c5e6f8cbc8180ba9359a6964fcf5586
Joely Brito <joely_brito@outlook.com>
2022-09-07 13:21:26 -0300

    Adicionado endpoint que cria usuário

--- modulo3/introducao-autenticacao/src/index.ts
+++ modulo3/introducao-autenticacao/src/index.ts
@@ -1,10 +1,37 @@
 import app from "./app"
 import editUser from './endpoints/editUser'
-import createUser from './endpoints/createUser'
+import createUser from './data/createUser'
 import { IdGenerator } from "./services/IdGeneration"
+import { Request, Response } from "express"
+import generateToken from "./services/generateToken"
 
 const generator = new IdGenerator().generateId()
 console.log(generator)
 
-app.post('/user/signup', createUser)
+app.post('/user/signup', async (req: Request, res: Response) => {
+    try {
+        if (!req.body.email || !req.body.email.includes("@")) {
+            throw new Error("Email inválido");
+        }
+        if (!req.body.password || req.body.password.length < 6) {
+            throw new Error("Senha fora do padrão exigido")
+        } 
+        const id = new IdGenerator().generateId()
+
+        const email = req.body.email
+        const password = req.body.password
+
+        await createUser(id, email, password)
+
+        const token = generateToken({id})
+        res.status(201).send({
+            message: "Usuário criado!", 
+            token: token
+        })
+    } catch (error: any) {
+        res.status(400).send({
+            message: error.message,
+        });
+    }
+});
 app.put('/user/edit/:id', editUser)
