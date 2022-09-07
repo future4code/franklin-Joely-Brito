39332452ce3490c9f723765c5b5f98b1baf66b1b
Joely Brito <joely_brito@outlook.com>
2022-09-07 12:28:23 -0300

    Adicionado arquivo tables

--- modulo3/introducao-autenticacao/tables.sql
+++ modulo3/introducao-autenticacao/tables.sql
@@ -22,3 +22,9 @@ CREATE TABLE IF NOT EXISTS to_do_list_assignees (
     FOREIGN KEY (task_id) REFERENCES to_do_list_tasks(id),
     FOREIGN KEY (assignee_id) REFERENCES to_do_list_users(id)
 );
+
+CREATE TABLE user (
+    id VARCHAR(255) PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
