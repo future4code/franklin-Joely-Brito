e92cd348210c08a66297c9db9517a0c71947ffb1
Joely Brito <joely_brito@outlook.com>
2022-09-14 14:22:29 -0300

    Adicionado metodos de criação

--- modulo3/arquitetura-de-software-1/src/database/UserDatabase.ts
+++ modulo3/arquitetura-de-software-1/src/database/UserDatabase.ts
@@ -2,24 +2,44 @@ import { IUserDB, User } from "../model/User";
 import { BaseDatabase } from "./BaseDatabase";
 
 export default class UserDatabase extends BaseDatabase {
-    public static TABLE_USERS = "Arq_Users"
+  public static TABLE_USERS = "Arq_Users";
 
-    public toUserDBModel = (user: User) => {
-        const userDB: IUserDB = {
-            id: user.getId(),
-            name: user.getName(),
-            email: user.getEmail(),
-            password: user.getPassword(),
-            role: user.getRole()
-        }
-        
-        return userDB
-    }
+  public toUserDBModel = (user: User) => {
+    const userDB: IUserDB = {
+      id: user.getId(),
+      name: user.getName(),
+      email: user.getEmail(),
+      password: user.getPassword(),
+      role: user.getRole(),
+    };
 
-    public createUser = async (user: User) => {
-        const userDB = this.toUserDBModel(user)
+    return userDB;
+  };
 
-        await BaseDatabase.connection(UserDatabase.TABLE_USERS)
-            .insert(userDB)
-    }
+  public createUser = async (user: User) => {
+    const userDB = this.toUserDBModel(user);
+
+    await BaseDatabase.connection(UserDatabase.TABLE_USERS).insert(userDB);
+  };
+
+  public getByEmail = async (email: string) => {
+    const result = await BaseDatabase.connection
+      .select("*")
+      .from(UserDatabase.TABLE_USERS)
+      .where("email", email);
+    return result[0];
+  };
+
+  public getAll = async () => {
+    const result = await BaseDatabase.connection
+      .select("*")
+      .from(UserDatabase.TABLE_USERS);
+    return result;
+  };
+
+  public deleteUser = async (id: string) => {
+    await BaseDatabase.connection(UserDatabase.TABLE_USERS)
+      .where("id", id)
+      .del();
+  };
 }
