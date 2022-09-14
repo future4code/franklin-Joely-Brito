9152d41dc1a46cc1505fa351f884921ab83a3d3a
Joely Brito <joely_brito@outlook.com>
2022-09-14 14:23:28 -0300

    Adicionado funções de login, getAll e deleteUser

--- modulo3/arquitetura-de-software-1/src/controller/UserController.ts
+++ modulo3/arquitetura-de-software-1/src/controller/UserController.ts
@@ -2,24 +2,75 @@ import { Request, Response } from "express";
 import UserBusiness from "../business/UserBusiness";
 
 export default class UserController {
-    public signup = async (req: Request, res: Response) => {
-        try {
-            const input: any = {
-                name: req.body.name,
-                email: req.body.email,
-                password: req.body.password
-            }
+  public signup = async (req: Request, res: Response) => {
+    try {
+      const input: any = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      };
 
-            const userBusiness = new UserBusiness()
-            const response = await userBusiness.signup(input)
-            
-            res.status(201).send(response)
-        } catch (error: unknown) {
-            if (error instanceof Error) {
-                return res.status(400).send({ message: error.message })
-            }
+      const userBusiness = new UserBusiness();
+      const response = await userBusiness.signup(input);
 
-            res.status(500).send({ message: "Erro inesperado"})
-        }
+      res.status(201).send(response);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return res.status(400).send({ message: error.message });
+      }
+
+      res.status(500).send({ message: "Erro inesperado" });
     }
+  };
+
+  public login = async (req: Request, res: Response) => {
+    try {
+      const input: any = {
+        email: req.body.email,
+        password: req.body.password,
+      };
+
+      const userBusiness = new UserBusiness();
+      const response = await userBusiness.login(input);
+
+      res.status(200).send(response);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return res.status(400).send({ message: error.message });
+      }
+
+      res.status(500).send({ message: "Erro inesperado" });
+    }
+  };
+
+  public getAll = async (req: Request, res: Response) => {
+    try {
+      const headers = req.headers.token;
+      const userBusiness = new UserBusiness();
+      const response = await userBusiness.getAll(headers as string);
+
+      res.status(200).send(response);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return res.status(401).send({ message: error.message });
+      }
+      res.status(500).send({ message: "Erro inesperado" });
+    }
+  };
+
+  public deleteUser = async (req: Request, res: Response) => {
+    try {
+      const headers = req.headers.token;
+      const id = req.params.id;
+      const userBusiness = new UserBusiness();
+      const response = await userBusiness.deleteUser(id, headers as string);
+
+      res.status(200).send(response);
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(401).send({ message: error.message });
+      }
+      res.status(500).send({ message: "Erro inesperado" });
+    }
+  };
 }
