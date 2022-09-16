3fb58a3ae61da874652267556a308f83878d46ed
Joely Brito <joely_brito@outlook.com>
2022-09-16 14:57:21 -0300

    Adicionado a tipagem dos dados vindos do input e saidas no output

--- modulo3/arquitetura-software-2/src/models/User.ts
+++ modulo3/arquitetura-software-2/src/models/User.ts
@@ -1,90 +1,120 @@
 export enum USER_ROLES {
-    NORMAL = "NORMAL",
-    ADMIN = "ADMIN"
+  NORMAL = "NORMAL",
+  ADMIN = "ADMIN",
 }
 
 export interface IUserDB {
-    id: string,
-    name: string,
-    email: string,
-    password: string,
-    role: USER_ROLES
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  role: USER_ROLES;
 }
 
 export class User {
-    constructor(
-        private id: string,
-        private name: string,
-        private email: string,
-        private password: string,
-        private role: USER_ROLES
-    ) { }
+  constructor(
+    private id: string,
+    private name: string,
+    private email: string,
+    private password: string,
+    private role: USER_ROLES
+  ) {}
 
-    public getId = () => {
-        return this.id
-    }
+  public getId = () => {
+    return this.id;
+  };
 
-    public getName = () => {
-        return this.name
-    }
+  public getName = () => {
+    return this.name;
+  };
 
-    public getEmail = () => {
-        return this.email
-    }
+  public getEmail = () => {
+    return this.email;
+  };
 
-    public getPassword = () => {
-        return this.password
-    }
+  public getPassword = () => {
+    return this.password;
+  };
 
-    public getRole = () => {
-        return this.role
-    }
+  public getRole = () => {
+    return this.role;
+  };
 
-    public setId = (newId: string) => {
-        this.id = newId
-    }
+  public setId = (newId: string) => {
+    this.id = newId;
+  };
 
-    public setName = (newName: string) => {
-        this.name = newName
-    }
+  public setName = (newName: string) => {
+    this.name = newName;
+  };
 
-    public setEmail = (newEmail: string) => {
-        this.email = newEmail
-    }
+  public setEmail = (newEmail: string) => {
+    this.email = newEmail;
+  };
 
-    public setPassword = (newPassword: string) => {
-        this.password = newPassword
-    }
+  public setPassword = (newPassword: string) => {
+    this.password = newPassword;
+  };
 
-    public setRole = (newRole: USER_ROLES) => {
-        this.role = newRole
-    }
+  public setRole = (newRole: USER_ROLES) => {
+    this.role = newRole;
+  };
 }
 
 export interface ISignupInputDTO {
-    name: string,
-    email: string,
-    password: string
+  name: string;
+  email: string;
+  password: string;
 }
 
 export interface ISignupOutputDTO {
-    message: string,
-    token: string
+  message: string;
+  token: string;
 }
 
+export interface ILoginInputDTO {
+  email: string;
+  password: string;
+}
+
+export interface ILoginOutputDTO {
+  message: string;
+  token: string;
+}
 export interface IGetUsersInputDTO {
-    token: string | undefined,
-    search: string,
-    order: string,
-    sort: string,
-    limit: string,
-    page: string,
+  token: string | undefined;
+  search: string;
+  order: string;
+  sort: string;
+  limit: string;
+  page: string;
 }
 
 export interface IGetUsersInputDBDTO {
-    search: string,
-    order: string,
-    sort: string,
-    limit: number,
-    offset: number,
+  search: string;
+  order: string;
+  sort: string;
+  limit: number;
+  offset: number;
+}
+
+export interface IDeleteUserInputDTO {
+  token: string;
+  idToDelete: string;
+}
+
+export interface IDeleteUserOutputDTO {
+  message: string;
+}
+
+export interface IEditeUserInputDTO {
+  token: string | undefined;
+  idToEdit: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface IEditeUserOutputDTO {
+    message: string;
 }
