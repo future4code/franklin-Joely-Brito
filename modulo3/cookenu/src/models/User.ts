dcf1611ca20e0f55729bc50b87cbc9317dd3f37b
Joely Brito <joely_brito@outlook.com>
2022-09-25 20:37:42 -0300

    Refatorado arquivo que faz a tipagem das entradas e saídas

--- modulo3/cookenu/src/models/User.ts
+++ modulo3/cookenu/src/models/User.ts
@@ -1,5 +1,40 @@
 export interface ISignupInputDTO {
-    name: string;
-    email: string;
-    password: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface ISignupOutputDTO {
+    message: string,
+    token: string,
+}
+export interface IUserDB {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+}
+export class User {
+  constructor(
+    private id: string,
+    private name: string,
+    private email: string,
+    private password: string
+  ) {}
+
+  public getId = () => {
+    return this.id;
+  };
+
+  public getName = () => {
+    return this.name;
+  };
+
+  public getEmail = () => {
+    return this.email;
+  };
+
+  public getPassword = () => {
+    return this.password;
+  };
 }
