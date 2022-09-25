6bb92964509958ac65fd411c355802ffa09a39f2
Joely Brito <joely_brito@outlook.com>
2022-09-25 20:38:12 -0300

    Refatorado arquivo que faz requisições ao banco de dados

--- modulo3/cookenu/src/database/UserDatabase.ts
+++ modulo3/cookenu/src/database/UserDatabase.ts
@@ -1,5 +1,28 @@
+import { IUserDB, User } from "../models/User"
 import { BaseDatabase } from "./BaseDatabase"
 
 export class UserDatabase extends BaseDatabase {
     public static TABLE_USERS = "Users_Cookenu"
+
+    public findByEmail = async (email: string) => {
+        const usersDB: IUserDB[] = await BaseDatabase
+        .connection(UserDatabase.TABLE_USERS)
+        .select()
+        .where({ email })
+
+        return usersDB[0]
+    }
+
+    public createUser = async (user: User) => {
+        const userDB: IUserDB = {
+            id: user.getId(),
+            name: user.getName(),
+            email: user.getEmail(),
+            password: user.getPassword(),
+        }
+
+        await BaseDatabase
+            .connection(UserDatabase.TABLE_USERS)
+            .insert(userDB)
+    }
 }
