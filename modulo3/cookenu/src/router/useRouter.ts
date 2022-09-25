ae7e430004c03555204e40b2b10074be74b40160
Joely Brito <joely_brito@outlook.com>
2022-09-25 20:36:15 -0300

    Refatorado arquivo que cuida das rotas

--- modulo3/cookenu/src/router/useRouter.ts
+++ modulo3/cookenu/src/router/useRouter.ts
@@ -1,4 +1,20 @@
-import { Router } from 'express'
+import { Router } from "express";
+import { UserBusiness } from "../business/UserBusiness";
+import { UserController } from "../controller/UserController";
+import { UserDatabase } from "../database/UserDatabase";
+import { Authenticator } from "../services/Authenticator";
+import { hashManager } from "../services/HasManager";
+import { idGenerator } from "../services/IdGenerator";
 
-export const userRouter = Router()
+export const userRouter = Router();
 
+const userController = new UserController(
+  new UserBusiness(
+    new UserDatabase(),
+    new idGenerator(),
+    new hashManager(),
+    new Authenticator()
+  )
+);
+
+userRouter.post("/signup", userController.signup);
