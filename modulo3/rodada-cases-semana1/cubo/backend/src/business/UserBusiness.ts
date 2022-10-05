d4ad2751522c7c3f19eafb0edd9a69c07cd61947
Joely Brito <joely_brito@outlook.com>
2022-10-05 15:46:32 -0300

    Reorganização das pastas e separação do front-end e back-end

---
+++ modulo3/rodada-cases-semana1/cubo/backend/src/business/UserBusiness.ts
@@ -0,0 +1,44 @@
+import { UserDatabase } from "../database/UserDatabase";
+import { ISignupInputDTO, User } from "../models/User";
+import { IdGenerator } from "../services/IdGenerator";
+
+export class userBusiness {
+  constructor(
+    protected userDatabase: UserDatabase,
+    protected idGenerator: IdGenerator
+  ) {}
+
+  public signup = async (input: ISignupInputDTO) => {
+    const first_name = input.first_name;
+    const last_name = input.last_name;
+    const participacion = input.participacion;
+
+    if (!first_name || !last_name || !participacion) {
+      throw new Error("Um ou mais parâmetros faltando");
+    }
+
+    if (
+      typeof first_name !== "string" ||
+      typeof last_name !== "string" ||
+      typeof participacion !== "string"
+    ) {
+      throw new Error("Um ou mais parâmetros inválidos");
+    }
+
+    const id = this.idGenerator.generate();
+
+    const user = new User(id, first_name, last_name, participacion);
+
+    await this.userDatabase.createUser(user);
+
+    const response = {
+      message: "Usuário cadastrado com sucesso!",
+    };
+    return response;
+  };
+
+  public getUsers = async () => {
+    const response = await this.userDatabase.getUsers();
+    return response;
+  };
+}
