eab88f1346906b0807a2870b673481626d1503c5
Joely Brito <joely_brito@outlook.com>
2022-10-06 20:09:33 -0300

    Adicionado styled ao header

---
+++ modulo3/rodada-cases-semana1/cubo/frontend/src/styled.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+export const Header = styled.header`
+height: 100px;
+width: 100%;
+background-color: #00B8E2;
+display: flex;
+align-items: center;
+justify-content: center;
+`
+export const Form = styled.form`
+display:flex;
+justify-content: space-between;
+
+& > div {
+margin-right: 25px;
+border-radius: 3px;
+border: none;
+background-color: white;
+}
+
+& > Button {
+    background-color: #00B8E2;
+    border: 1.5px solid white;
+    color: white;
+    width: 100px
+
+}
+`
