15e881c1047b95cc4e22aa35b6d3cc8e91d0af60
Joely Brito <joely_brito@outlook.com>
2022-10-06 20:10:36 -0300

    Criação do header

--- modulo3/rodada-cases-semana1/cubo/frontend/src/App.js
+++ modulo3/rodada-cases-semana1/cubo/frontend/src/App.js
@@ -1,6 +1,21 @@
+import { Header, Form } from "./styled.js";
+import TextField from '@mui/material/TextField';
+import Button from "@mui/material/Button";
+
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Header>
+        <Form>
+        <TextField id="filled-basic" label="First name" variant="outlined" />
+        <TextField id="filled-basic" label="Last name" variant="outlined" />
+        <TextField id="filled-basic" label="Participation" variant="outlined" />
+        <Button variant="outlined">Send</Button>
+        </Form>
+      </Header>
+    </>
+  );
 }
 
 export default App;
