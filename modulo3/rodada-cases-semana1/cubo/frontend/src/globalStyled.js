fb09a612d4bfbeeb080e20faf1479569ab11d49b
Joely Brito <joely_brito@outlook.com>
2022-10-06 20:06:57 -0300

    Adicionado estilo global ao projeto

---
+++ modulo3/rodada-cases-semana1/cubo/frontend/src/globalStyled.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+export const GlobalStyled = createGlobalStyle`
+ * {
+    box-sizing: border-box;
+    margin: 0px;
+    padding: 0px;
+ }
+
+ body {
+    font-family: 'Roboto', sans-serif;
+ }
+ `;
