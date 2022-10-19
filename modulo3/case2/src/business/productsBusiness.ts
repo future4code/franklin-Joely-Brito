a7396291c372d74850ccc2e1938ee4f1f7686539
Joely Brito <joely_brito@outlook.com>
2022-10-19 13:51:14 -0300

    Adicionado classe ProductsBusiness

--- modulo3/case2/src/business/productsBusiness.ts
+++ modulo3/case2/src/business/productsBusiness.ts
@@ -1,9 +1,19 @@
 import { ProductsDatabase } from "../database/ProductsDatabase";
+import { products } from "../model/Products";
 
 export class ProductsBusiness {
-    constructor(
-        protected procuctsDatabase: ProductsDatabase
-    ) {}
-
-    
+  constructor(protected productsDatabase: ProductsDatabase) {}
+  public createProducts = async (normalizeProducts: products[]) => {
+    normalizeProducts.forEach((product) => {
+        console.log(product)
+      if (!product.id || !product.name || !product.tags) {
+        throw new Error("Um ou mais parâmetros faltando!");
+      }
+    });
+    await this.productsDatabase.createProducts(normalizeProducts);
+    const response = {
+      message: "Produto cadastrado com sucesso!",
+    };
+    return response;
+  };
 }
