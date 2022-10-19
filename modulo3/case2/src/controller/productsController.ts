17371e1314d8519c77233f66375d4f3e8dadce50
Joely Brito <joely_brito@outlook.com>
2022-10-19 20:23:18 -0300

    Refatorado a entrada de dados

--- modulo3/case2/src/controller/productsController.ts
+++ modulo3/case2/src/controller/productsController.ts
@@ -5,15 +5,27 @@ export class ProductsController {
   constructor(protected productsBusiness: ProductsBusiness) {}
   public registration = async (req: Request, res: Response) => {
     try {
-      const products = req.body.products;
-      const normalizeProducts = products.map((product: any) => {
-        return {
-          id: Number(product.id),
-          name: product.name,
-          tags: product.tags.toString(),
-        };
-      });
-
+      let normalizeProducts;
+      if (req.get("Content-Type") === "application/xml") {
+        const products = req.body.products.element;
+        normalizeProducts = products.map((product: any) => {
+          const tags = product.tags[0].element.toString();
+          return {
+            id: Number(product.id.toString()),
+            name: product.name.toString(),
+            tags,
+          };
+        });
+      } else {
+        const productsJson = req.body.products;
+        normalizeProducts = productsJson.map((product: any) => {
+          return {
+            id: Number(product.id),
+            name: product.name,
+            tags: product.tags.toString(),
+          };
+        });
+      }
       const response = await this.productsBusiness.createProducts(
         normalizeProducts
       );
