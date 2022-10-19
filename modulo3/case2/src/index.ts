f6cd44c18a405328865eae4e4520759c8183c990
Joely Brito <joely_brito@outlook.com>
2022-10-19 20:22:46 -0300

    Refatorado index

--- modulo3/case2/src/index.ts
+++ modulo3/case2/src/index.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import xmlparser from 'express-xml-bodyparser'
 import { userRouter } from './router/userRouter'
 
 dotenv.config()
 
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded())
+app.use(xmlparser())
 app.use(cors())
 
 app.listen(process.env.PORT || 3003, () => {
