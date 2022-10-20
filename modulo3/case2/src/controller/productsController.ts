78ead278cd4063d481d2c3afcd17cf86088e5926
Joely Brito <joely_brito@outlook.com>
2022-10-20 19:43:36 -0300

    Adicionado tratamento dos dados enviados pelo usuário

--- modulo3/case2/src/controller/productsController.ts
+++ modulo3/case2/src/controller/productsController.ts
@@ -9,10 +9,10 @@ export class ProductsController {
       if (req.get("Content-Type") === "application/xml") {
         const products = req.body.products.element;
         normalizeProducts = products.map((product: any) => {
-          const tags = product.tags[0].element.toString();
+          const tags = product.tags[0].element.toString().toLowerCase();
           return {
             id: Number(product.id.toString()),
-            name: product.name.toString(),
+            name: product.name.toString().toLowerCase(),
             tags,
           };
         });
@@ -21,8 +21,8 @@ export class ProductsController {
         normalizeProducts = productsJson.map((product: any) => {
           return {
             id: Number(product.id),
-            name: product.name,
-            tags: product.tags.toString(),
+            name: product.name.toLowerCase(),
+            tags: product.tags.toString().toLowerCase(),
           };
         });
       }
@@ -56,8 +56,24 @@ export class ProductsController {
 
   public getProductsByName = async (req: Request, res: Response) => {
     try {
-      const name = req.params.name;
+      const name = req.params.name.toLowerCase();
       const response = await this.productsBusiness.getProductsByName(name);
+
+      res.status(200).send(response);
+    } catch (error) {
+      console.log(error);
+
+      if (error instanceof Error) {
+        return res.status(400).send({ message: error.message });
+      }
+      res.status(500).send({ message: "Erro inesperado" });
+    }
+  };
+
+  public getProductsByTags = async (req: Request, res: Response) => {
+    try {
+      const tags = req.params.tags.toLowerCase();
+      const response = await this.productsBusiness.getProductsByTags(tags);
 
       res.status(200).send(response);
     } catch (error) {
